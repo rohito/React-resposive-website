ab5440a853b20b39148eb9e38ac2b0627dd3e88b
rohito <tamurohit@gmail.com>
2020-10-18 17:08:01 +0100

    navbar and sidebar

---
+++ src/components/HeroSection/HeroElements.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeroContainer = styled.div`
+    background:#0c0c0c;
+    display:flex;
+    justify-content: center;
+    align-items:center;
+    
+`
