e54f4377b1865e67fd23e3510f1624a388da5965
rohito <tamurohit@gmail.com>
2020-10-18 18:39:31 +0100

    video background and buttons

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Video from '../../videos/video.mp4'
+import { Button } from '../ButtonElement'
+import { ArrowForward, ArrowRight, HeroBg, HeroBtnWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './HeroElements'
 
 const HeroSection = () => {
+
+    const [hover,setHover]= useState(false)
+
+    const onHover = ()=>{
+        setHover(!hover)
+    }
     return (
         <HeroContainer >
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive £250 in credit towards your next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" onMourseEnter={onHover} onMouseLeave={onHover} >
+                        Get started {hover ? <ArrowForward/>: <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
